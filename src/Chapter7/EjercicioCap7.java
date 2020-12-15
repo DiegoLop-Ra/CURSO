d46913963111736b2b4952361ee207f3cefec070
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-14 20:05:50 -0600

    ActualizacionEjercicioCap7

--- src/Chapter7/EjercicioCap7.java
+++ src/Chapter7/EjercicioCap7.java
@@ -1,2 +1,17 @@
-package Chapter7;public class EjercicioCap7 {
+package Chapter7;
+
+import java.util.Scanner;
+
+public class EjercicioCap7 {
+
+    public static void main(String args[]){
+        String[] semana = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingresa un numero del 1 al 7");
+        int index = input.nextInt();
+        input.close();
+
+        System.out.println("Ese numero corresponde al dia: " + semana[index - 1]);
+    }
 }
