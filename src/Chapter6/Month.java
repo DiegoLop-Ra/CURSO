ba5d66489110a05f9a8433012c4270f87e889ec5
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-14 19:54:22 -0600

    ActualizacionMESES

--- src/Chapter6/Month.java
+++ src/Chapter6/Month.java
@@ -1,2 +1,40 @@
-package Chapter6;public class Month {
+package Chapter6;
+
+public class Month {
+
+    public static String getMonth(int month){
+        switch(month){
+            case 1: return "Enero";
+            case 2: return "Febrero";
+            case 3: return "Marzo";
+            case 4: return "Abril";
+            case 5: return "Mayo";
+            case 6: return "Junio";
+            case 7: return "Julio";
+            case 8: return "Agosto";
+            case 9: return "Septiembre";
+            case 10: return "Octubre";
+            case 11: return "Noviembre";
+            case 12: return "Diciembre";
+            default: return "Mes Inixistente. Por favor ingresa un valor del 1 al 12.";
+        }
+    }
+
+    public static int getMonth(String month){
+        switch(month){
+            case "Enero": return 1;
+            case "Febrero": return 2;
+            case "Marzo": return 3;
+            case "Abril": return 4;
+            case "Mayo": return 5;
+            case "Junio": return 6;
+            case "Julio": return 7;
+            case "Agosto": return 8;
+            case "Septiembre": return 9;
+            case "Octubre": return 10;
+            case "Noviembre": return 11;
+            case "Diciembre": return 12;
+            default: return -1;
+        }
+    }
 }
