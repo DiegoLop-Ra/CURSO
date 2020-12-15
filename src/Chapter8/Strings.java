0727a444cae6a4040929af33928b86cee9a0b377
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-14 20:09:49 -0600

    ActualizacionStrings

--- src/Chapter8/Strings.java
+++ src/Chapter8/Strings.java
@@ -1,2 +1,26 @@
-package Chapter8;public class Strings {
+package Chapter8;
+
+public class Strings {
+
+    public static void main(String[] args){
+        countWords("Me gustan las manzanas");
+    }
+
+    /**
+     * Splits a String into an array by tokenizing it.
+     * Counts words and prints them
+     * @param text Full string to be split
+     */
+    public static void countWords(String text){
+        var words = text.split(" ");
+        int numberOfWords = words.length;
+
+        String message = String.format("Tu texto contiene %d palabras:", numberOfWords);
+        System.out.println(message);
+
+        for(int i=0; i<numberOfWords; i++){
+            System.out.println(words[i]);
+        }
+    }
+
 }
