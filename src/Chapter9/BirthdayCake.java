c9fb8293ae58652840373ec3a146ce47af937b0a
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-14 20:11:43 -0600

    ActualizacionPastel

--- src/Chapter9/BirthdayCake.java
+++ src/Chapter9/BirthdayCake.java
@@ -1,2 +1,18 @@
-package Chapter9;public class BirthdayCake {
+package Chapter9;
+
+public class BirthdayCake extends Cake{
+
+    private int candles;
+
+    public BirthdayCake(){
+        super("vanilla");
+    }
+
+    public int getCandles() {
+        return candles;
+    }
+
+    public void setCandles(int candles) {
+        this.candles = candles;
+    }
 }
