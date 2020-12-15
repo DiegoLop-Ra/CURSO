82cfe8aff6b264b5e864dca727d404d319609d28
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-14 20:29:04 -0600

    ActualizacionDivision

--- src/Chapter13/DivisionCero.java
+++ src/Chapter13/DivisionCero.java
@@ -1,2 +1,15 @@
-package Chapter13;public class DivisionCero {
+package Chapter13;
+
+public class DivisionCero {
+
+    public static void main(String[] args){
+
+        try{
+            int c = 30/0;
+        }catch(ArithmeticException e){
+            System.out.println("La division entre cero no esta permitida");
+        }finally{
+            System.out.println("Dividir es divertido!");
+        }
+    }
 }
