3c4c1cbe46ae6aafcd39a5be4cb60111bf815fb7
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-14 20:09:30 -0600

    ActualizacionRevertir

--- src/Chapter8/Revertir.java
+++ src/Chapter8/Revertir.java
@@ -1,2 +1,18 @@
-package Chapter8;public class Revertir {
+package Chapter8;
+
+public class Revertir {
+
+    public static void main(String[] args){
+        reverseString("Caballero");
+    }
+
+    /**
+     * Prints a String in reverse order
+     * @param text String to reverse
+     */
+    public static void reverseString(String text){
+        for(int i=text.length()-1; i>=0; i--){
+            System.out.print(text.charAt(i));
+        }
+    }
 }
