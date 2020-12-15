8079d3c26d0d71adcac409538548893416f38451
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-14 20:17:50 -0600

    ActualizacionPlatano

--- src/Chapter10/Banana.java
+++ src/Chapter10/Banana.java
@@ -1,2 +1,17 @@
-package Chapter10;public class Banana {
+package Chapter10;
+
+public class Banana extends Fruit {
+
+    public Banana(){
+        setCalories(105);
+    }
+
+    public void peel(){
+        System.out.println("Banana has been peeled");
+    }
+
+    @Override
+    public void makeJuice() {
+        System.out.println("Banana juice is gross 🥴");
+    }
 }
