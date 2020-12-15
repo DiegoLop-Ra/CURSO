c17aafa70569f9364d7ce57ca385436479a05dd4
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-14 20:12:26 -0600

    ActualizacionEmpleado

--- src/Chapter9/Employee.java
+++ src/Chapter9/Employee.java
@@ -1,2 +1,23 @@
-package Chapter9;public class Employee {
+package Chapter9;
+
+public class Employee extends Person {
+
+    private String employeeId;
+    private String title;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
