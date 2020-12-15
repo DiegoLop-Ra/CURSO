21a2ab4bced17123f009fef4956c3b6b2db51b28
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-14 19:45:03 -0600

    ActualizacionPrimerMEtodo

--- src/Chapter5/PrimerMetodo.java
+++ src/Chapter5/PrimerMetodo.java
@@ -1,2 +1,19 @@
-package Chapter5;public class PrimerMetodo {
+package Chapter5;
+
+import java.util.Scanner;
+
+public class PrimerMetodo {
+
+    public static void main(String args[]){
+
+        System.out.println("Ingresa tu nombre:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        greetUser(name);
+    }
+
+    public static void greetUser(String name){
+
+        System.out.println("Hola, " + name);
+    }
 }
