2c576c05509a29655731c7edaf236ca3a0b0cd61
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-14 20:30:21 -0600

    ActualizacionMoneda

--- src/Chapter14/Coin.java
+++ src/Chapter14/Coin.java
@@ -1,2 +1,29 @@
-package Chapter14;public class Coin {
+package Chapter14;
+
+import java.util.Random;
+
+public class Coin {
+
+    private String side;
+    public static String HEADS = "Heads";
+    public static String TAILS = "Tails";
+
+    public String getSide() {
+        return side;
+    }
+
+    public void setSide(String side) {
+        this.side = side;
+    }
+
+    public String flip(){
+        if(new Random().nextBoolean()){
+            setSide(HEADS);
+        }
+        else{
+            setSide(TAILS);
+        }
+
+        return getSide();
+    }
 }
