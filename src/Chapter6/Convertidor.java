e8bc05800f1361cce291d0179a89a2a00d93aa4a
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-14 19:59:22 -0600

    ActualizacionConvertidor

--- src/Chapter6/Convertidor.java
+++ src/Chapter6/Convertidor.java
@@ -1,2 +1,8 @@
-package Chapter6;public class Convertidor {
+package Chapter6;
+
+public class Convertidor {
+    public static void main(String args[]){
+        System.out.println(Month.getMonth(5));
+        System.out.println(Month.getMonth("Marzo"));
+    }
 }
