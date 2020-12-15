315fe7eff77de15eb55a3c97d0e7ab82cb88da7f
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-14 20:18:07 -0600

    ActualizacionFruta

--- src/Chapter10/Fruit.java
+++ src/Chapter10/Fruit.java
@@ -1,2 +1,18 @@
-package Chapter10;public class Fruit {
+package Chapter10;
+
+public class Fruit {
+
+    private int calories;
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public void makeJuice(){
+        System.out.println("Juice is made");
+    }
 }
