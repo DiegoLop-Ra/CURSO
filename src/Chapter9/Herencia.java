3679c245b3be701e5392134c1603a0b6349bfb53
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-14 20:12:47 -0600

    ActualizacionHerencia

--- src/Chapter9/Herencia.java
+++ src/Chapter9/Herencia.java
@@ -1,2 +1,11 @@
-package Chapter9;public class Herencia {
+package Chapter9;
+
+public class Herencia {
+
+    public static void main(String[] args){
+
+        Person person = new Person();
+        Employee employee = new Employee();
+
+    }
 }
